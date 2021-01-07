88b03b0daf1a9b4975a8976486c908b9e27df0b5
Dominic Padula <domopadula2192@gmail.com>
2021-01-07 10:27:16 -0700

    took 30 mins to realize I wasnt returning an accumulator fun

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,9 +1,11 @@
 class Museum
   attr_reader:name,
-             :exhibits
+             :exhibits,
+             :patrons
   def initialize(name)
     @name = name
     @exhibits = []
+    @patrons = []
   end
 
   def add_exhibit(exhibit)
@@ -13,6 +15,25 @@ class Museum
   def recommend_exhibits(patron)
     @exhibits.find_all do |exhibit|
        patron.interests.include?(exhibit.name)
+    end
+  end
+
+  def admit(patron)
+    @patrons << patron
+  end
+
+  def patron_interests(exhibit)
+    @patrons.find_all do |patron|
+      patron.interests.include?(exhibit.name)
+    end
+  end
+
+  def patrons_by_exhibit_interest
+    @exhibits.reduce({}) do |acc, exhibit|
+      if acc[exhibit].nil?
+        acc[exhibit] = patron_interests(exhibit)
+      end
+      acc
     end
   end
 end
