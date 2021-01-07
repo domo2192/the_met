e49bb3d242415f4d5510d04f8fec96153fe47eec
Dominic Padula <domopadula2192@gmail.com>
2021-01-07 09:16:11 -0700

    Fix type and tests for interests array

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -10,7 +10,14 @@ class PatronTest < Minitest::Test
 
   def test_it_exists_and_has_attributes
     assert_instance_of Patron, @patron_1
-    assert_equal "Bob", @patron.name
-    assert_equal 20, @patron.spending_money 
+    assert_equal "Bob", @patron_1.name
+    assert_equal 20, @patron_1.spending_money
+  end
+
+  def test_patrons_start_with_an_empty_array_of_interests
+    assert_equal [], @patron_1.interests
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_1.add_interest("Gems and Minerals")
+    assert_equal ["Dead sea Scrolls", "Gems and Minirals"]
   end
 end
