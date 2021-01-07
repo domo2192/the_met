754ccd0b529407b621b3aa5c772677b7f744aaef
Dominic Padula <domopadula2192@gmail.com>
2021-01-07 09:33:25 -0700

    Passing code for recommend_exhibits and typos in test

--- lib/museum.rb
+++ lib/museum.rb
@@ -7,7 +7,12 @@ class Museum
   end
 
   def add_exhibit(exhibit)
-    @exhibits << exhibit 
+    @exhibits << exhibit
+  end
 
+  def recommend_exhibits(patron)
+    @exhibits.find_all do |exhibit|
+       patron.interests.include?(exhibit.name)
+    end
   end
 end
--- test/museum_test.rb
+++ test/museum_test.rb
@@ -36,8 +36,8 @@ class Test < Minitest::Test
     @patron_1.add_interest("Dead Sea Scrolls")
     @patron_1.add_interest("Gems and Minerals")
     @patron_2.add_interest("IMAX")
-    assert_equal [@dead_sea_scrolls, @gems_and_minerals], @dmns.recomment_exhibits(@patron_1)
-    assert_equal [@imax], @dmns.recomment_exhibits(@patron_2)
+    assert_equal [@gems_and_minerals, @dead_sea_scrolls], @dmns.recommend_exhibits(@patron_1)
+    assert_equal [@imax], @dmns.recommend_exhibits(@patron_2)
   end
 
 end
